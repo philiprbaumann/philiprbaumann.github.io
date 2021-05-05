9b34a6959923c7520d59a4ce17bc0c1e255c1cca
philiprbaumann <77937811+philiprbaumann@users.noreply.github.com>
2021-05-04 21:59:47 -0400

    fixing music dropdowns

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,7 @@
 $(document).ready(function() {
 
-	$('.nav-toggle').click(function(e) {
-		
-		e.preventDefault();
-		$("html").toggleClass("openNav");
-		$(".nav-toggle").toggleClass("active");
-
-	});
-
-	$("li").on("mouseover", function() {
-		$("li").removeClass("active");
-		$(this).addClass("active");
+	$('.music-link').click(function(e) {
+		$(this).next().toggle();;
 	});
 	
 });
